d32902d82a8970d0c47a2070c20f5e862c437682
maarithoo <99177602+maarithoo@users.noreply.github.com>
2022-02-15 18:16:35 +0200

    Uusi versio

---
+++ random_image2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang=fi>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <title>Satunnainen koirakuva</title>
+	</head>
+	<body>
+        <h1>Satunnainen koirakuva</h1>
+
+    <?php
+
+        $kuvat = array (
+
+        $koira0 = array ("images/koira0.jpg", "Juno tötterö päässä", "600", "400"),
+                
+        $koira1 = array ("images/koira1.jpg", "Pixi ja Juno kalliolla", "600", "400"),
+
+        $koira2 = array ("images/koira2.jpg", "Pixi lempisiilinsä kanssa", "600", "400"),
+
+        $koira3 = array ("images/koira3.jpg", "Pixi ja Juno pedissä", "600", "400")
+        );
+
+        $randomLuku = rand(0,count($kuvat)-1);
+        
+        echo '<img src="' . $kuvat[$randomLuku][0] . '"alt="' . $kuvat[$randomLuku][1] . '"height="' . $kuvat[$randomLuku][2] . '"width="' . $kuvat[$randomLuku][3] .'"/>';
+        echo "<br>"
+        
+    ?>
+    
+    <button type="button" id="reload" onClick="window.location.reload();">Lataa uusi kuva</button>
+          
+    </body>
+</html>
---
+++ random_image3.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang=fi>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <title>Satunnainen koirakuva</title>
+	</head>
+	<body>
+        <h1>Satunnainen koirakuva</h1>
+
+    <?php
+        
+        $kuvat = array (
+
+        $koira0 = array ("src"=>"images/koira0.jpg",
+                "alt"=>"Juno tötterö päässä",
+                "width"=>"400 px",
+                "height" =>"600 px"),
+
+        $koira1 = array ("src"=>"images/koira0.jpg",
+                "alt"=>"Pixi ja Juno kalliolla",
+                "width"=>"400 px",
+                "height"=>"600 px"),
+
+        $koira2 = array ("src"=>"images/koira0.jpg",
+                "alt"=>"Pixi lempisiilinsä kanssa",
+                "width"=>"400 px",
+                "height" =>"600 px"),
+
+        $koira3 = array ("src"=>"images/koira0.jpg",
+                "alt"=>"Pixi ja Juno pedissä",
+                "width"=>"400 px",
+                "height" =>"600 px"),
+        );
+        
+        $randomLuku = rand(0,count($kuvat)-1);
+
+        echo '<img src="' . $kuvat[$randomLuku]['src'] . '"alt="' . $kuvat[$randomLuku]['alt'] . '"/>';
+        echo "<br>"
+        
+    ?>
+    
+    <button type="button" id="reload" onClick="window.location.reload();">Lataa uusi kuva</button>
+          
+    </body>
+</html>
