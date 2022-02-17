017784eb049549fbb4f25695b7d451bb1f6749a3
maarithoo <99177602+maarithoo@users.noreply.github.com>
2022-02-17 10:26:13 +0200

    Update foreach.php

--- foreach.php
+++ foreach.php
@@ -4,8 +4,13 @@
 $numerot = array(4,7,5,8,6,13,9,1,3,11,2,14,16,10,12,15); 
 
 foreach ($numerot as $value) {
-    echo "$value <br>";
+    if ($value > 8) {
+        echo $value . "<br>";
+        $suuretNumerot[] = $value;
+    }
 }
+
+print_r($suuretNumerot);
 
 /*Lisää silmukkaan if-lause jotta se tulostaa vain luvut jotka ovat suurempia kuin 8.
 Bonus: luo tulostettavista numeroista uusi taulukkomuuttuja (esim. $suuretNumerot= …)*/
