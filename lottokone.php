cd7bb7e04da7b1ebf19a733de6da93ccb2c948fe
maarithoo <99177602+maarithoo@users.noreply.github.com>
2022-02-17 13:28:49 +0200

    Create lottokone.php

---
+++ lottokone.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang=fi>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <title>Lottokone</title>
+	</head>
+	<body>
+        <h1>Lottokone</h1>
+
+        <form action="lottokone.php" method="post">
+        Syötä nimesi: <input type="text" name="nimi"><br><br>
+        Syötä osoite: <input type="text" name="osoite"><br><br>
+        <input type="submit" value="Lähetä">
+        </form>
+
+    </body>
+</html>    
