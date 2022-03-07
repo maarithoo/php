d11106852ee559a03494c18da3919dd7373af0c3
maarithoo <99177602+maarithoo@users.noreply.github.com>
2022-03-07 20:57:38 +0200

    Update mietelause.php

--- mietelause/mietelause.php
+++ mietelause/mietelause.php
@@ -11,29 +11,33 @@
 <body>
  
 <?php
-// aukaistaan tietokantayhteys
-$tietokantayhteys = 
-  mysqli_connect("", "kurssi", "kood10tus", "php2022");
+    // aukaistaan tietokantayhteys
+    $tietokantayhteys = 
+    mysqli_connect("", "kurssi", "kood10tus", "php2022");
 
-// tarkista yhteysvirheet
-if (mysqli_connect_errno()) {
-  echo "Yhteysvirhe tietokantaan: " . mysqli_connect_error();
-}
-// Asetetaan merkistö jotta ääkköset näkyvät
-mysqli_set_charset($tietokantayhteys,"utf8");
+    // tarkista yhteysvirheet
+    if (mysqli_connect_errno()) {
+        echo "Yhteysvirhe tietokantaan: " . mysqli_connect_error();
+    }
+    // Asetetaan merkistö jotta ääkköset näkyvät
+    mysqli_set_charset($tietokantayhteys,"utf8");
 
-// Haetaan tietokantataulusta satunnainen aforismi
-$sqlkom = "SELECT * FROM maarit_aforismit ORDER BY RAND() LIMIT 1";
-$hakutulos = mysqli_query($tietokantayhteys, $sqlkom);
+    // Haetaan tietokantataulusta satunnainen aforismi
+    $sqlkom = "SELECT * FROM maarit_aforismit ORDER BY RAND() LIMIT 1";
+    $hakutulos = mysqli_query($tietokantayhteys, $sqlkom);
 
-// Haetaan arraysta halutut arvot loopilla
-while ($rivi = mysqli_fetch_array($hakutulos))
-{   
+    // Haetaan arraysta halutut arvot loopilla
+    $rivi = mysqli_fetch_array($hakutulos);
+    
+    // Tulostetaan halutut taulukkoarvot css:llä määriteltyihin diveihin, kategoria-arvojen taustaväri muuttuu näytetyn aforismin mukaan, kts. myös vaihtoehtoinen if-lauseella toteutus allaolevissa kommentoissa.
     echo "<div class='flex-container'>";
         echo "<main class='main'>$rivi[3]</main><br>";
         echo "<nav class='byline'>Lähde:<br>$rivi[2]</nav><br>";
+        echo "<aside class=" . $rivi[1] . "> Kategoria:<br>" . $rivi[1] . "</aside><br>"; 
 
-    // Vaihdetaan divin taustaväri näytetyn kategorian mukaan
+
+    /* Tehtävänannossa kehotettiin käyttämään if-lausetta, eli tein ensin tämän version taustavärin vaihtumisesta. Kuitenkin ylempänä oleva vaihtoehto on toteutettu yksinkertaisemmin, joten jätin sen voimaan.
+
     if ($rivi[1] == "Oppiminen") {
         $kategoria = "Oppiminen"; 
         }    
@@ -48,12 +52,11 @@ while ($rivi = mysqli_fetch_array($hakutulos))
         } 
         elseif ($rivi[1] == "Onnellisuus") {
             $kategoria = "Onnellisuus"; 
-        }
+        } 
+        echo "<aside class=" . $rivi[1] . "> Kategoria:<br>" . $rivi[1] . "</aside><br>"; */
         
-        echo "<aside class=" . $kategoria . "> Kategoria:<br>" . $rivi[1] . "</aside><br>"; 
-        
-        echo "</div>";
-}
+    echo "</div>";
+
 ?>
     <footer>
         <button type="button" class="reload" onClick="window.location.reload();">Lue jokin toinen ajatelma</button>
